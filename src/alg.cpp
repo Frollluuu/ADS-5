2e2be25bb4415b32c5bbb3535d4a1b329b8c154e
Frollluuu <Frolovaannet2004@yandex.ru>
2024-04-07 14:01:49 +0300

    1

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,74 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int priority(char op) {
+    if (op == '*' || op == '/') {
+        return 2;
+    } else if (op == '+' || op == '-') {
+        return 1;
+    } else {
+        return 0; // для скобок
+    }
 }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+std::string infx2pstfx(std::string inf) {
+    TStack<char, 100> stack;
+    std::string pst = "";
+    
+    for (char &c : inf) {
+        if (c == ' ') {
+            continue; // пропускаем пробелы
+        }
+
+        if (isdigit(c)) {
+            pst += c;
+        } else if (c == '(') {
+            stack.push(c);
+        } else if (c == ')') {
+            while (stack.get() != '(') {
+                pst += " ";
+                pst += stack.pop();
+            }
+            stack.pop();
+        } else {
+            pst += " ";
+            while (!stack.isEmpty() && priority(stack.get()) >= priority(c)) {
+                pst += stack.pop();
+            }
+            stack.push(c);
+        }
+    }
+    
+    while (!stack.isEmpty()) {
+        pst += " ";
+        pst += stack.pop();
+    }
+    
+    return pst;
+}
+
+int eval(std::string post) {
+    TStack<int, 100> stack;
+    std::istringstream ss(post);
+    std::string token;
+    
+    while (ss >> token) {
+        if (isdigit(token[0])) {
+            stack.push(std::stoi(token));
+        } else {
+            int b = stack.pop();
+            int a = stack.pop();
+            if (token == "+") {
+                stack.push(a + b);
+            } else if (token == "-") {
+                stack.push(a - b);
+            } else if (token == "*") {
+                stack.push(a * b);
+            } else if (token == "/") {
+                stack.push(a / b);
+            }
+        }
+    }
+    
+    return stack.get();
 }
